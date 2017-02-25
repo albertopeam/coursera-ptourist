dcc4a7126dd08fe79ccc898e3ed63a3c70588822
Alberto Penas Amor <alberto__pa@hotmail.com>
2017-02-25 13:16:33 +0100

    added show

--- app/assets/javascripts/spa-demo/subjects/types/types.component.js
+++ app/assets/javascripts/spa-demo/subjects/types/types.component.js
@@ -9,11 +9,22 @@
       bindings: {
         authz: "<"
       }
+    })
+    .component("sdTypesEditor", {
+      templateUrl: typeEditorTemplateUrl,
+      controller: TypeEditorController,
+      bindings: {
+        authz: "<"
+      }
     });
 
   typeSelectorTemplateUrl.$inject = ["spa-demo.config.APP_CONFIG"];
   function typeSelectorTemplateUrl(APP_CONFIG) {
     return APP_CONFIG.type_selector_html;
+  }
+  typeEditorTemplateUrl.$inject = ["spa-demo.config.APP_CONFIG"];
+  function typeEditorTemplateUrl(APP_CONFIG) {
+    return APP_CONFIG.type_editor_html;
   }
 
   TypeSelectorController.$inject = ["$scope",
@@ -22,7 +33,7 @@
                                      "spa-demo.subjects.Type"];
   function TypeSelectorController($scope, $stateParams, Authz, Type) {
     var vm=this;
-    
+
     vm.$onInit = function() {
       console.log("TypeSelectorController",$scope);
       $scope.$watch(function(){ return Authz.getAuthorizedUserId(); },
@@ -35,5 +46,59 @@
     return;
     //////////////
   }
+
+  TypeEditorController.$inject = ["$scope","$q",
+                                   "$state", "$stateParams",
+                                   "spa-demo.authz.Authz",
+                                   "spa-demo.subjects.Type"
+                                   ];
+  function TypeEditorController($scope, $q, $state, $stateParams,
+                                 Authz, Type) {
+    var vm=this;
+
+    vm.$onInit = function() {
+      console.log("TypeEditorController",$scope);
+      $scope.$watch(function(){ return Authz.getAuthorizedUserId(); },
+                    function(){
+                      console.log("TypeEditorController-watch-getAuthorizedUserId");
+                      if ($stateParams.id) {
+                        reload($stateParams.id);
+                      } else {
+                        //newResource();
+                      }
+                    });
+    }
+    return;
+    //////////////
+
+    function reload(typeId) {
+      var itemId = typeId ? typeId : vm.item.id;
+      console.log("TypeEditorController-re/loading type", itemId);
+      vm.item = Type.get({id:itemId});
+      $q.all([vm.item.$promise])
+        .then(function(result){
+          console.log("TypeEditorController-", result);
+        })
+        .catch(handleError);
+    }
+
+    function clear() {
+      console.log("TypeEditorController-clear");
+      $state.go(".", {id:null});
+    }
+
+    function handleError(response) {
+      console.log("TypeEditorController-error", response);
+      if (response.data) {
+        vm.item["errors"]=response.data.errors;
+      }
+      if (!vm.item.errors) {
+        vm.item["errors"]={}
+        vm.item["errors"]["full_messages"]=[response];
+      }
+      //$scope.typeform.$setPristine();
+    }
+  }
+
 
 })();
